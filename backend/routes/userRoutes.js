2f384f6526c4725df39dafe72c350d5c199e0f1d
rzounlome <rzounlome@harmonyhit.com>
2021-01-04 11:44:56 -0500

    added user update route to backend

--- backend/routes/userRoutes.js
+++ backend/routes/userRoutes.js
@@ -1,10 +1,18 @@
 import express from "express";
 const router = express.Router();
-import {authUser, getUserProfile, registerUser} from '../controllers/userController'
-import protect from '../middleware/authMiddleware'
+import {
+  authUser,
+  getUserProfile,
+  registerUser,
+  updateUserProfile,
+} from "../controllers/userController";
+import protect from "../middleware/authMiddleware";
 
-router.route('/login').post(authUser)
-router.route('/profile').get(protect, getUserProfile)
-router.route('/').post(registerUser)
+router.route("/login").post(authUser);
+router
+  .route("/profile")
+  .get(protect, getUserProfile)
+  .put(protect, updateUserProfile);
+router.route("/").post(registerUser);
 
 export default router;
